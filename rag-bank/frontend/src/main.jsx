aea6ef0a27f5257a8d5f46584b497e83f0ac48b8
Satvik Sharma <sharmasatvik031@gmail.com>
2026-02-08 17:24:44 +0530

    bootstrap icons added

--- rag-bank/frontend/src/main.jsx
+++ rag-bank/frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css' // Your custom CSS comes last
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
